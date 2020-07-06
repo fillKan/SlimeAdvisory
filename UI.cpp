d6dddaddfc1ffddedde6075183e168b22ee1c9ed
한대건 <han15050424@gmail.com>
2020-07-06 15:39:56 +0900

    BUGFIX : Ui를 출력할때 한가지의 이미지만 출력하는 버그와 이미지가 비스듬히 출력되는 버그를 해결한다
    이미지가 비슴듬히 출력되는 버그의 원인은 회전값에 1.f를 넣었기 때문에 그랬던 것이고, 한가지의 이미지만
    출력하는 버그는...FillImage를 출력해야할때 BackImage를 출력하는 것이 문제였다. 이래서 중복된 코드란..

--- UI.cpp
+++ UI.cpp
@@ -92,18 +92,18 @@ void UserInterface::Render(Vector2 pos, Image* image, RECT renderingRECT)
 
 	if (image->BackImage)
 	{
-		D3DXMatrixAffineTransformation2D(&matrix, 1.0f, nullptr, 1.f, &pos);
+		D3DXMatrixAffineTransformation2D(&matrix, 1.0f, nullptr, 0.f, &pos);
 
 		IMAGE->GetSprite()->SetTransform(&matrix);
 		IMAGE->GetSprite()->Draw(image->BackImage->pTexture, nullptr, nullptr, nullptr, D3DCOLOR_XRGB(255,255,255));
 	}
 	if (image->FillImage)
 	{
-		IMAGE->GetSprite()->Draw(image->BackImage->pTexture, &renderingRECT, nullptr, nullptr, D3DCOLOR_XRGB(255, 255, 255));
+		IMAGE->GetSprite()->Draw(image->FillImage->pTexture, &renderingRECT, nullptr, nullptr, D3DCOLOR_XRGB(255, 255, 255));
 	}
 	if (image->EdgeImage)
 	{
-		IMAGE->GetSprite()->Draw(image->BackImage->pTexture, nullptr, nullptr, nullptr, D3DCOLOR_XRGB(255, 255, 255));
+		IMAGE->GetSprite()->Draw(image->EdgeImage->pTexture, nullptr, nullptr, nullptr, D3DCOLOR_XRGB(255, 255, 255));
 	}
 }
 
