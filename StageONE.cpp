ddc81685939f6444ecc043cd4050552f364d313e
한대건 <han15050424@gmail.com>
2020-07-11 19:04:32 +0900

    구름 스크롤링을 진행하도록 한다
    와~우~프로세스 메모리 100MB-!
    
    구름들의 위치와 텍스처는 2개씩 존재하기 때문에, 이것을 쉽게? 관리하기 위해서 배열로 바꾸었다. 더불어, 변수(지금은 배열)의 이름또한 간략하게 바꾸었다.
    구름들은 고정된 수치로 왼쪽을 향해서 움직이며, 움직이는 속도는 구름의 크기가 작을수록 느리다. 렌더링되는 순서는 작은구름부터 렌더링 된다.

--- StageONE.cpp
+++ StageONE.cpp
@@ -19,14 +19,18 @@ void StageONE::Init()
 	USER_INTERFACE->AddUI(new PHealthBar());
 	USER_INTERFACE->AddUI(mButton);
 
-	mCloudHuge[0] = IMAGE->AddImage("CloudHuge", "./image/BackGround/Cloud/Cloud1.png");
-	mCloudHuge[1] = mCloudHuge[0];
+	mHCloud[0] = IMAGE->AddImage("CloudHuge", "./image/BackGround/Cloud/Cloud1.png");
+	mHCloud[1] = mHCloud[0];
 
-	mCloudMiddle[0] = IMAGE->AddImage("CloudMiddle", "./image/BackGround/Cloud/Cloud2.png");
-	mCloudMiddle[1] = mCloudMiddle[0];
+	mMCloud[0] = IMAGE->AddImage("CloudMiddle", "./image/BackGround/Cloud/Cloud2.png");
+	mMCloud[1] = mMCloud[0];
 
-	mCloudSmall[0] = IMAGE->AddImage("CloudSmall", "./image/BackGround/Cloud/Cloud3.png");
-	mCloudSmall[1] = mCloudSmall[0];
+	mSCloud[0] = IMAGE->AddImage("CloudSmall", "./image/BackGround/Cloud/Cloud3.png");
+	mSCloud[1] = mSCloud[0];
+
+	mHCloudPos[0] = ZERO; mHCloudPos[1] = Vector2(WINSIZEX, 0);
+	mMCloudPos[0] = ZERO; mMCloudPos[1] = Vector2(WINSIZEX, 0);
+	mSCloudPos[0] = ZERO; mSCloudPos[1] = Vector2(WINSIZEX, 0);
 }
 
 void StageONE::Update()
@@ -35,20 +39,31 @@ void StageONE::Update()
 	{
 		OBJECT->AddObject(new DummyEnemy());
 	}
+
+	for (int i = 0; i < 2; ++i)
+	{
+		mHCloudPos[i] += LEFT *   HUGE_CLOUD_SPEED;
+		mMCloudPos[i] += LEFT * MIDDLE_CLOUD_SPEED;
+		mSCloudPos[i] += LEFT *  SMALL_CLOUD_SPEED;
+
+		if (mHCloudPos[i].x <= -WINSIZEX) { mHCloudPos[i].x = WINSIZEX; }
+		if (mMCloudPos[i].x <= -WINSIZEX) { mMCloudPos[i].x = WINSIZEX; }
+		if (mSCloudPos[i].x <= -WINSIZEX) { mSCloudPos[i].x = WINSIZEX; }
+	}
 }
 
 void StageONE::Render()
 {
 	mBackGround->Render(ZERO);
 
-	mCloudHuge[0]->Render(ZERO);
-	//mCloudHuge[1]->Render(ZERO);
+	mSCloud[0]->Render(mSCloudPos[0]);
+	mSCloud[1]->Render(mSCloudPos[1]);
 
-	mCloudMiddle[0]->Render(ZERO);
-	//mCloudMiddle[1]->Render(ZERO);
+	mMCloud[0]->Render(mMCloudPos[0]);
+	mMCloud[1]->Render(mMCloudPos[1]);
 
-	mCloudSmall[0]->Render(ZERO);
-	//mCloudSmall[1]->Render(ZERO);
+	mHCloud[0]->Render(mHCloudPos[0]);
+	mHCloud[1]->Render(mHCloudPos[1]);
 }
 
 void StageONE::Release()
