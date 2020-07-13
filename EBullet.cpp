c82750f7a12a7b457ce2c9e94df0d9806de519f2
한대건 <han15050424@gmail.com>
2020-07-13 09:32:43 +0900

    적이 발사하는 총탄 클래스 EBullet을 추가한다
    현재 추가한 EBullet는 PBullet와 별 다를게 없다..다만 생성자를 더 많이 사용할 뿐..생성자를 많이 사용하는 이유는, 다양한 적들이 이 총탄을
    사용할 것인데, 그 총탄마다 약간의 변화를 줘야한다고 생각하기 때문에 그렇다.

---
+++ EBullet.cpp
@@ -0,0 +1,50 @@
+#include "DXUT.h"
+#include "EBullet.h"
+
+EBullet::EBullet(Vector2 pos, Vector2 dir, float speed) : mSpeed(speed)
+{
+	Position  = pos;
+	Direction = dir;
+}
+
+EBullet::~EBullet()
+{
+}
+
+void EBullet::Init()
+{
+	Name = "EBullet";
+	Tag = TAG::EBULLET;
+
+	mAnimation.AddFrame("./image/Enemy/Bullet/", "MonsterBullet", 6);
+
+	CircleRadius = 6.f;
+}
+
+void EBullet::Update()
+{
+	Velocity = ZERO;
+
+	Position += (Velocity += mSpeed * Direction);
+}
+
+void EBullet::Render()
+{
+	IMAGE->CenterRender(mAnimation.Play(), Position);
+}
+
+void EBullet::Release()
+{
+}
+
+void EBullet::OnCollisionEnter(Object* other)
+{
+}
+
+void EBullet::OnCollisionStay(Object* other)
+{
+}
+
+void EBullet::OnCollisionExit(Object* other)
+{
+}
---
+++ EBullet.h
@@ -0,0 +1,22 @@
+#pragma once
+class EBullet : public Object
+{
+public:
+	 EBullet(Vector2 pos, Vector2 dir, float speed);
+	~EBullet();
+
+	virtual void Init   () override;
+	virtual void Update () override;
+	virtual void Render () override;
+	virtual void Release() override;
+
+	virtual void OnCollisionEnter(Object* other) override;
+	virtual void OnCollisionStay (Object* other) override;
+	virtual void OnCollisionExit (Object* other) override;
+
+private:
+	float mSpeed;
+
+	Animation mAnimation;
+};
+
