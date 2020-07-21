299f89c2fe071e20471c1e046901fa4836f02e90
한대건 <han15050424@gmail.com>
2020-07-22 08:54:03 +0900

    적이 총탄을 생성하는 위치를 조정하고, 가끔식 발생하는 오류를 수정한다
    가끔씩 발생하는 오류라는 것은, 플레이어의 소멸자가 실행되면서 파티클 객체를 제거하는 과정에서 파티클이 nullptr이 되는 것으로
    플레이어의 소멸자가 실행이 파티클이 소멸되기 이전에 실행되도록 하여 이 오류를 수정한다.
    근데 이 오류가 발생하는 원인을 잘 몰르겠어서 제대로 고친것인지도 좀 의아하다..
    
    그리고 적이 총탄을 생성하는 위치를 자연스러워 보이게끔 옮겨준다.

--- DummyEnemy.cpp
+++ DummyEnemy.cpp
@@ -43,7 +43,7 @@ void DummyEnemy::Update()
 
 		if (player)
 		{
-			OBJECT->AddObject(new EBullet(Position, player->Position, 630.f));
+			OBJECT->AddObject(new EBullet(Position + Vector2(-55, 24), player->Position, 630.f));
 		}
 	}
 }
