5bfeffe75ecf5bc0a2ab957882bbda15f5366a00
한대건 <han15050424@gmail.com>
2020-07-21 19:05:51 +0900

    플레이어 코드를 뒤집는다
    변경사항은 다음과 같다 :
    
    - 플레이어의 이동코드를 간결하고, 쉽게 알아볼 수 있도록 수정
    - 멤버변수 이름의 변경을 cpp코드에 적용
    - 플레이어의 공격 코드가 눈에 더욱 잘 들어올 수 있게끔 공백을 추가했고, 사용하는 지역변수의 이름이 역할을 드러내도록 수정
    - 플레이어이 공격 파티클은 Update함수 내에서 생성되는 방식을 Init에서 생성하도록 수정
    - 일부 코드를 영역별로 region을 달아서 구분을 둠
    
    와! 변경사항 짱 많네요 저 변경사항들은 나름의 리팩토링의 과정입니다 좋지않은 코드는 쳐내는 그런 뭐

--- Player.cpp
+++ Player.cpp
@@ -9,35 +9,43 @@
 
 Player::Player() : mCircleShout(1.3f), mBlank(1.3f), mSteamPack(2.75f, 6.f, 2.f)
 {
-	mAttackParticle = nullptr;
-	mBoostEffect    = nullptr;
+	mEnergyCube  = nullptr;
+	mBoostEffect = nullptr;
 }
 
 Player::~Player()
 {
 	mAnimation.Clear();
 
-	if (mAttackParticle != nullptr)
+	if (mEnergyCube != nullptr)
 	{
-		mAttackParticle->SetDestroy(true);
+		mEnergyCube->SetDestroy(true);
 	}
 }
 
 void Player::Init()
 {
+#pragma region 플레이어 능력치 설정
+
 	Name = "Player";
 	Tag = TAG::PLAYER;
 
 	Position = Vector2(WINSIZEX / 2, WINSIZEY / 2);
-	mAttackPoint = (Position + Vector2(130.5f, 20.5f));
+	mATKpoint = Position + ATK_POINT_OFFSET;
 
 	mAnimation.SetFrame("./image/Player/Default/", "Player", 18);
 
 	CircleRadius = 50.f;
 
 	mSpeed = 15.f;
+	
+	HealthInit(10.f);
+
+#pragma endregion
 
 	mATKcool.SetTimer(0.15f, true);
+
+#pragma region 플레이어가 공격할때 나오는 이펙트 객체 설정
 
 	mBoostEffect = new PBoost();
 	OBJECT->AddObject(mBoostEffect);
@@ -45,7 +53,9 @@ void Player::Init()
 	this->Child = mBoostEffect;
 	mBoostEffect->Parent = this;
 
-	HealthInit(10.f);
+	mEnergyCube = PARTICLE->Instantiate(PARTICLES::PATTACK, mATKpoint);
+
+#pragma endregion
 
 	SkillGauge* skillGauge = new SkillGauge(Vector2(55.f, 210.f));
 	skillGauge->SkillLink(&mBlank);
@@ -64,6 +74,8 @@ void Player::Init()
 
 void Player::Update()
 {
+	mATKpoint = Position + ATK_POINT_OFFSET;
+
 	if (CURHealth <= 0)
 	{
 		SCENCE->LoadScence("Title");
@@ -84,53 +96,33 @@ void Player::Update()
 		mSteamPack.CastSkill();
 	}
 
-	if (INPUT->GetKeyDown(ATTACKKEY) && mAttackParticle == nullptr)
-	{
-		mAttackParticle = PARTICLE->Instantiate(PARTICLES::PATTACK, mAttackPoint);
-	}
 	if (INPUT->GetKey(ATTACKKEY) && mATKcool.TimeOver())
 	{
-		Object* cloestObj = OBJECT->ForwardCloest(Position, TAG::ENEMY);
+		Object* target = OBJECT->ForwardCloest(Position, TAG::ENEMY);
 
 		Vector2 dir = RIGHT;
-		float rot = 0.f;
+		float   rot = 0.00f;
 
-		if (cloestObj != nullptr)
+		if (target != nullptr)
 		{
-			dir = Math::AimVector(cloestObj->Position, mAttackPoint);
+			dir = Math::AimVector(target->Position, mATKpoint);
 
-			rot = Math::RadianAngle(mAttackPoint, cloestObj->Position);
+			rot = Math::RadianAngle(mATKpoint, target->Position);
 		}
-		OBJECT->AddObject(new PBullet(mAttackPoint, dir, rot));
+		OBJECT->AddObject(new PBullet(mATKpoint, dir, rot));
 	}
+
 	Vector2 velocity = ZERO;
 
-	if (GetAsyncKeyState('W'))
-	{
-		if (Position.y > 0) velocity += UP * mSpeed;
-	}
-	else if (GetAsyncKeyState('S'))
-	{
-		if (Position.y < WINSIZEY) velocity += DOWN * mSpeed;
-	}
-	if (GetAsyncKeyState('D'))
-	{
-		if (Position.x < WINSIZEX) velocity += RIGHT * mSpeed;
-	}
-	else if (GetAsyncKeyState('A'))
-	{
-		if (Position.x > 0) velocity += LEFT * mSpeed;
-	}
-	Position += velocity;
+	velocity += (UP    * mSpeed * INPUT->GetKey('W')) + (DOWN * mSpeed * INPUT->GetKey('S')) +
+				(RIGHT * mSpeed * INPUT->GetKey('D')) + (LEFT * mSpeed * INPUT->GetKey('A')) + Position;
 
-	mAttackPoint = (Position + ATK_PARTICLE_OFFSET);
+	if (velocity.x > 0 && velocity.x < WINSIZEX) { Position.x = velocity.x; }
+	if (velocity.y > 0 && velocity.y < WINSIZEY) { Position.y = velocity.y; }
 
-	mBoostEffect->Position = (Position + BOOST_OFFSET);
 
-	if (mAttackParticle)
-	{
-		mAttackParticle->Position = mAttackPoint;
-	}
+	mBoostEffect->Position = Position + BOOST_OFFSET;
+	 mEnergyCube->Position = mATKpoint;
 }
 
 void Player::Render()
